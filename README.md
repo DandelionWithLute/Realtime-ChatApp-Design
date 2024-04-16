bc15d437c23fbb10368debf6fe12d3945e831207
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-04-16 15:02:35 +0800

    userContainer

--- README.md
+++ README.md
@@ -7,9 +7,13 @@ https://youtu.be/domt_Sx-wTY?si=UwgtlJZyuMOEIp4A
 check CSS accessibility
 caniuse.com
 
+backdrop
+背景幕布
 
 accessibility
 可及性
 
 feasibility
 可行性，可能性
+
+box-shadow: 3px 3px .4em lightcyan,-3px -3px .4em lightcyan;
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 const App = () => {
   return (
-    <div className='container'></div>
-  )
-}
+    <div className="container">
+      {/* ChatList */}
+      <div className="chatList">
+        <div className="userCard">
+          <img src="avatar.png" className="" />
+          <div className="userName">
+            <a>Dandelion</a>
+          </div>
+          <div className="userIcons">
+            <img src="more.png" />
+            <img src="video.png" />
+            <img src="edit.png" />
+          </div>
+        </div>
+        <div className="">{/* <img src="plus.png" /> */}</div>
+      </div>
+      {/* ChatContainer */}
+      <div className="chatContainer">chatContainer</div>
+      {/* ChatDetails */}
+      <div className="chatDetails">chatDetails</div>
+    </div>
+  );
+};
 
-export default App
+export default App;
