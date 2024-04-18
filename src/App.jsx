fb23d5a3d7a37ebf11bc771defe4c7576ff93771
OrangeBird <yuanxinbj@gmail.com>
2024-04-19 03:01:49 +0800

    main

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,53 @@ const App = () => {
             </div>
           </div>
           {/* userContactListContainer */}
-          <div className="userContactListContainer">123</div>
+          <div className="userContactListContainer">
+            {/* 1 */}
+            <div className="userCard">
+              <div className="avatarContainer">
+                <img src="avatar.png" className="" />
+              </div>
+              <div className="userName">
+                <a>Dandelion</a>
+              </div>
+              {/* <div className="userIcons">
+                <img src="more.png" />
+                <img src="video.png" />
+                <img src="edit.png" />
+              </div> */}
+            </div>
+            {/* 2 */}
+            <div className="userCard">
+              <div className="avatarContainer">
+                <img src="avatar.png" className="" />
+              </div>
+              <div className="userNameAndDescContainer">
+                <div className="userName">
+                  <a>Dandelion</a>
+                </div>
+                <div className="desc">hello</div>
+              </div>
+              {/* <div className="userIcons">
+                <img src="more.png" />
+                <img src="video.png" />
+                <img src="edit.png" />
+              </div> */}
+            </div>
+            {/* 3 */}
+            <div className="userCard">
+              <div className="avatarContainer">
+                <img src="avatar.png" className="" />
+              </div>
+              <div className="userName">
+                <a>Dandelion</a>
+              </div>
+              {/* <div className="userIcons">
+                <img src="more.png" />
+                <img src="video.png" />
+                <img src="edit.png" />
+              </div> */}
+            </div>
+          </div>
         </div>
       </div>
       <div className="mainVerticalSeparator">
@@ -39,6 +85,9 @@ const App = () => {
       </div>
       {/* ChatContainer */}
       <div className="chatContainer">chatContainer</div>
+      <div className="mainVerticalSeparator">
+        <div className="mainVerticalSeparatorMiddle"></div>
+      </div>
       {/* ChatDetails */}
       <div className="chatDetails">chatDetails</div>
     </div>
